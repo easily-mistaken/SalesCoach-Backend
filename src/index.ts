b4de4e68c5243f3fdd0d9c021a1bc388794ffb86
Sharathxct <sharathxct@gmail.com>
2025-03-21 17:37:57 +0530

    Schema and migrations

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ const app = express();
 // Enable CORS
 app.use(cors());
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
 // Start server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
