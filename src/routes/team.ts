eb01d53298a238d37d68a32efc317a122bf255cf
Sharathxct <sharathxct@gmail.com>
2025-03-23 23:02:36 +0530

    one to many rel for org and teams

--- src/routes/team.ts
+++ src/routes/team.ts
@@ -1,2 +1,8 @@
 // create team route
 // invite user to team route
+// get all teams
+// get a team by id route
+// delete a team by id route
+// add user to team route
+// remove user from team route
+// get all users in a team route
