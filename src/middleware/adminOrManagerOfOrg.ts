ddea184d4161270208f4d49a24ad71584a1b46ba
Sharathxct <sharathxct@gmail.com>
2025-03-24 07:25:21 +0530

    create team

--- src/middleware/adminOrManagerOfOrg.ts
+++ src/middleware/adminOrManagerOfOrg.ts
@@ -3,16 +3,17 @@ import { NextFunction, Request, Response } from 'express';
 export const adminOrManagerOfOrg = (req: Request, res: Response, next: NextFunction): void => {
     // @ts-ignore
     const user = req.user;
-    const { organizationId } = req.params;
+    const organizationId = req.params.organizationId || req.body.organizationId;
     
-    if (user.role !== 'ADMIN' && user.role !== 'MANAGER') {
+    if (user.role != 'ADMIN' && user.role != 'MANAGER') {
+        console.log('user is not admin or manager');
         res.status(401).json({ message: 'Unauthorized' });
         return;
     }
 
-    // check if the user is part of the organization
-    const userOrganization = user.organizations.find((organization: any) => organization.organizationId === organizationId);
+    const userOrganization = user.organizations.find((organization: any) => organization.organizationId.toString() == organizationId);
     if (!userOrganization) {
+        console.log('user is not part of the organization');
         res.status(401).json({ message: 'Unauthorized' });
         return;
     }
