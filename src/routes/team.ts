3cee60694dc63d3b00600cc4116ccacbccc3bc78
easily-mistaken <choubeyprajjwal@gmail.com>
2025-03-30 20:47:18 +0530

    add: team routes

--- src/routes/team.ts
+++ src/routes/team.ts
@@ -1,76 +1,98 @@
-import { Router, Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const teamRouter = Router();
 
 // create team route
-teamRouter.post('/', async (req: Request, res: Response): Promise<void> => {
-    // @ts-ignore
-    const user = req.user;
-    const { name, description, organizationId } = req.body;
+teamRouter.post("/", async (req: Request, res: Response): Promise<void> => {
+  // @ts-ignore
+  const user = req.user;
+  if (user.role !== "ADMIN" || user.role !== "MANAGER") {
+    res
+      .status(401)
+      .json({ message: "You are not authorized to perform this action" });
+    return;
+  }
 
-    const team = await prisma.team.create({
-                    data: {
-                        name,
-                        description,
-                        organizationId,
-                    },
-                });
+  // user belongs to the organization
+  const { name, description, organizationId } = req.body;
 
-    res.status(201).json({ message: 'Team created', team });
+  const team = await prisma.team.create({
+    data: {
+      name,
+      description,
+      organizationId,
+    },
+  });
+
+  res.status(201).json({ message: "Team created", team });
 });
 
 // invite user to team route
 // TODO: send email to user with link to signup
-teamRouter.post('/invite', async (req: Request, res: Response): Promise<void> => {
+teamRouter.post(
+  "/invite",
+  async (req: Request, res: Response): Promise<void> => {
     const { email, teamId, role } = req.body;
     // @ts-ignore
     const user = req.user;
 
     const invite = await prisma.invite.create({
-        data: { email, teamId, role, invitedBy: user.id, organizationId: user.organizationId },
+      data: {
+        email,
+        teamId,
+        role,
+        invitedBy: user.id,
+        organizationId: user.organizationId,
+      },
     });
 
     // send email to the invited user
 
-    res.status(201).json({ message: 'Invite sent' });
-});
+    res.status(201).json({ message: "Invite sent" });
+  }
+);
 
 // get all teams
 // TODO: different auth for this route as this returns roles based output
-teamRouter.get('/', async (req: Request, res: Response): Promise<void> => {
-    const { organizationId } = req.query;
+teamRouter.get("/", async (req: Request, res: Response): Promise<void> => {
+  const { organizationId } = req.query;
 
-    const teams = await prisma.team.findMany({
-        where: {
-            organizationId: Array.isArray(organizationId) ? organizationId[0] : organizationId,
-        },
-    });
+  const teams = await prisma.team.findMany({
+    where: {
+      organizationId: Array.isArray(organizationId)
+        ? organizationId[0]
+        : organizationId,
+    },
+  });
 
-    res.json(teams);
+  res.json(teams);
 });
 
 // get a team by id route
-teamRouter.get('/:id', async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
+teamRouter.get("/:id", async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params;
 
-    const team = await prisma.team.findUnique({
-        where: { id: id },
-    });
+  const team = await prisma.team.findUnique({
+    where: { id: id },
+  });
 
-    res.json(team);
+  res.json(team);
 });
 
 // delete a team by id route
-teamRouter.delete('/:id', async (req: Request, res: Response): Promise<void> => {
+teamRouter.delete(
+  "/:id",
+  async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
 
     await prisma.team.delete({ where: { id: id } });
 
-    res.json({ message: 'Team deleted' });
-});
+    res.json({ message: "Team deleted" });
+  }
+);
 
 // remove user from team route
 
