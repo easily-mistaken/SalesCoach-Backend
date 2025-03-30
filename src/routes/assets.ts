c5af847f82fe85857c365d6477613643657f784f
Sharathxct <sharathxct@gmail.com>
2025-03-30 21:38:12 +0530

    clean up the routes

--- src/routes/assets.ts
+++ src/routes/assets.ts
@@ -17,7 +17,6 @@ assetsRouter.post('/', async (req: Request, res: Response): Promise<void> => {
             time,
             status,
             transcript,
-            teamId,
             userId,
         },
     });
@@ -36,37 +35,5 @@ assetsRouter.get('/', async (req: Request, res: Response): Promise<void> => {
     res.status(200).json({ assets });
 });
 
-// get assets of a team by id
-assetsRouter.get('/team/:id', async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-
-    const assets = await prisma.callAsset.findMany({
-        where: { teamId: id },
-    });
-
-    res.status(200).json({ assets });
-});
-
-// get assets of a team by name
-assetsRouter.get('/team/name/:name', async (req: Request, res: Response): Promise<void> => {
-    const { name } = req.params;
-
-    const assets = await prisma.callAsset.findMany({
-        where: { team: { name } },
-    });
-
-    res.status(200).json({ assets });
-});
-
-// get assets of a team by organization id
-assetsRouter.get('/team/organization/:id', async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-
-    const assets = await prisma.callAsset.findMany({
-        where: { team: { organizationId: id } },
-    });
-
-    res.status(200).json({ assets });
-});
 
 export default assetsRouter;
