e6ac19a4abaf1d09a1fdf5eb504c20b646c0b4d3
easily-mistaken <choubeyprajjwal@gmail.com>
2025-04-08 15:05:16 +0530

    fix: authMiddleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,60 +1,68 @@
-import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 // Middleware to fetch user details and store it in req.user
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // Fetch both token cookies
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  // Fetch both token cookies
 
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
 
-        if (!token) {
-            console.log("no token");
-            res.status(401).json({ message: 'Unauthorized' });
-            return;
+    if (!token) {
+      console.log("no token");
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    // Verify the access token
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET as string,
+      async (err: any, decoded: any) => {
+        if (err) {
+          res.status(401).json({ message: "Unauthorized" });
+          console.log("invalid token");
+          return;
         }
 
-        // Verify the access token
-        jwt.verify(token, process.env.JWT_SECRET as string, async (err: any, decoded: any) => {
-            if (err) {
-                res.status(401).json({ message: 'Unauthorized' });
-                console.log("invalid token")
-                return;
-            }
-
-            // Fetch user from the database using Prisma
-            const user = await prisma.user.findUnique({
-                where: { id: decoded.sub },
-                include: {
-                    organizations: {
-                        include: {
-                            organization: {
-                                include: {
-                                    callAssets: true
-                                }
-                            },
-                            teamAccess: true,
-                        }
-                    }
+        // Fetch user from the database using Prisma
+        const user = await prisma.user.findUnique({
+          where: { id: decoded.sub },
+          include: {
+            organizations: {
+              include: {
+                organization: {
+                  include: {
+                    callAssets: true,
+                  },
                 },
-            });
-
-            if (!user) {
-                res.status(401).json({ message: 'Unauthorized' });
-                console.log("no user")
-                return;
-            }
-
-            // @ts-ignore
-            req.user = user;
-            next();
+                teamAccess: true,
+              },
+            },
+          },
         });
-    } catch (error) {
-        console.log('Error parsing token:', error);
-        res.status(500).json({ message: 'Internal server error' });
-        return;
-    }
+
+        if (!user) {
+          res.status(401).json({ message: "Unauthorized" });
+          console.log("no user");
+          return;
+        }
+
+        // @ts-ignore
+        req.user = user;
+        next();
+      }
+    );
+  } catch (error) {
+    console.log("Error parsing token:", error);
+    res.status(500).json({ message: "Internal server error" });
+    return;
+  }
 };
